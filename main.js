38344f200d37b18f67622d0f0fc9efa6e86a8ad9
Italo Renan <italorenan225@gmail.com>
2023-03-23 15:19:46 -0300

    added textarea

--- main.js
+++ main.js
@@ -9,4 +9,13 @@ const createWindow = () => {
 	win.loadFile('index.html');
 };
 
-app.whenReady().then(() => createWindow());
+app.whenReady().then(() => {
+	createWindow();
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit();
+});
